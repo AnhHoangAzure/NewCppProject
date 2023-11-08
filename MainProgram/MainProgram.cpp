4d2742345ae7256f70e3dfe32d4c13368c9e0c5b
Anh Hoang <anh.comdr@gmail.com>
2023-11-08 06:51:52 +0200

    Update main

--- MainProgram/MainProgram.cpp
+++ MainProgram/MainProgram.cpp
@@ -1,20 +1,61 @@
-// MainProgram.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
-#include <iostream>
-
-int main()
-{
-    std::cout << "Hello World!\n";
+#include<iostream>
+#include<vector>
+using namespace std;
+bool PositiveOrNegativeChecking(int number) {
+	return (number >= 0) ? true : false;
+}
+int findGreatedNumber(vector<int> a) {
+	int greatestNumber = a[0];
+	for (int i = 0; i < a.size(); i++) {
+		if (a[i] >= greatestNumber) {
+			greatestNumber = a[i];
+		}
+	}
+	return greatestNumber;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+int solution(vector<int> a) {
+	vector<int> b;
+	for (int i = 0; i < a.size(); i++) {
+		for (int j = 0; j < a.size(); j++) {
+			if (a[i] == a[j]) {
+				b.push_back(j);
+				break;
+			}
+		}
+	}
+	if (b.size() == 0)
+		return -1;
+	for (int i = 0; i < b.size(); i++) {
+		for (int j = i + 1; j < b.size(); j++) {
+			if (b[j] < b[i]) {
+				int tmp = b[i];
+				b[i] = b[j];
+				b[j] = tmp;
+			}
+		}
+	}
+	return a[b[0]];
+}
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+int main() {
+	int number = -10;
+	int input;
+	if (PositiveOrNegativeChecking(number)) {
+		cout << number << " is positive" << endl;
+	}
+	else {
+		cout << number << " is negative" << endl;
+	}
+	vector<int> a;
+	cout << "Insert the 1st number: " << endl;
+	cin >> input;
+	a.push_back(input);
+	cout << "Insert the 2nd number: " << endl;
+	cin >> input;
+	a.push_back(input);
+	cout << "Insert the 3rd number: " << endl;
+	cin >> input;
+	a.push_back(input);
+	cout << "Greatest number: " << findGreatedNumber(a) << endl;
+}
